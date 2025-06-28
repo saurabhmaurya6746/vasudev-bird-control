ebaa34eafa2c24e45797525ba9cda8944a1c0840
Saurabh Maurya <saurabhmauryajnp28@gmail.com>
2025-06-28 16:56:03 +0530

    Update settings.py

--- birdcontrol/settings.py
+++ birdcontrol/settings.py
@@ -122,13 +122,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "myapp/static"),
+    os.path.join(BASE_DIR, 'myapp/static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
