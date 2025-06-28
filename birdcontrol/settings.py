3e888ce92a69dd98c9396989cae496f3146cfc9c
Saurabh Maurya <saurabhmauryajnp28@gmail.com>
2025-06-28 18:53:56 +0530

    Update settings.py

--- birdcontrol/settings.py
+++ birdcontrol/settings.py
@@ -146,10 +146,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # email configuration 
 # settings.py
+ '
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'  # or your SMTP host
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False 
 EMAIL_HOST_USER = 'vashudevbirdcontrol@gmail.com'
 EMAIL_HOST_PASSWORD = 'zaoe gxic nsqt lptl'
