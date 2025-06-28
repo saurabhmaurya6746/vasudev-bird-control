53b74f9307bb32f51046fe87f3eb1eda1aa74f9f
Saurabh Maurya <saurabhmauryajnp28@gmail.com>
2025-06-28 13:04:39 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
 
 echo "BUILD START"
-pip install -r requirement.txt
 
-# Run Django collectstatic
+# Install dependencies
+pip install -r requirements.txt
+
+# Collect static files
 python manage.py collectstatic --noinput
 
 echo "BUILD END"
