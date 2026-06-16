ccb9b3be0c9d6fd0afe44a5cfd4ead1195903801
Saurabh Maurya <saurabhmauryajnp28@gmail.com>
2026-06-16 08:50:52 +0530

    Update urls.py

--- birdcontrol/urls.py
+++ birdcontrol/urls.py
@@ -12,7 +12,8 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""from django.contrib import admin
+"""
+from django.contrib import admin
 from django.urls import path, include
 from django.contrib.sitemaps.views import sitemap
 from myapp.sitemaps import StaticViewSitemap
