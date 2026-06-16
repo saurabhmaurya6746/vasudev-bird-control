c47bb1bc9a61d6989779718c5a0f989064db9695
Saurabh Maurya <saurabhmauryajnp28@gmail.com>
2026-06-16 08:31:54 +0530

    Update sitemaps.py

--- myapp/sitemaps.py
+++ myapp/sitemaps.py
@@ -1,23 +1,22 @@
 from django.contrib.sitemaps import Sitemap
-from django.urls import reverse
 
 class StaticViewSitemap(Sitemap):
     priority = 0.8
-    changefreq = 'weekly'
+    changefreq = "weekly"
 
     def items(self):
         return [
-            'index',
-            'about',
-            'contact',
-            'bird-netting-services',
-            'bird-spike-system',
-            'bird-netting-services-delhi',
-            'bird-netting-services-varanasi',
-            'bird-netting-services-prayagraj',
-            'bird-netting-services-lucknow',
-            'bird-netting-services-jaunpur',
+            '/',
+            '/about/',
+            '/contact/',
+            '/bird-netting-services/',
+            '/bird-spike-system/',
+            '/bird-netting-services-delhi/',
+            '/bird-netting-services-varanasi/',
+            '/bird-netting-services-prayagraj/',
+            '/bird-netting-services-lucknow/',
+            '/bird-netting-services-jaunpur/',
         ]
 
     def location(self, item):
-        return reverse(item)
+        return item
